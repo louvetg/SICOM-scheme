c29fe29b34dbefd551b59a2f7a76be8baddf6341
megbrst <megane.barsanti@wanadoo.fr>
2016-11-19 15:55:47 +0100

    Update Primitive.c 19.11 16h00

--- src/primitive.c
+++ src/primitive.c
@@ -18,7 +18,7 @@
 */
 
 object* moins (object* o){
-	if (o == obj_empty_list)
+	if (o == obj_empty_list) // Cas liste vide
 	{ 
 		WARNING_MSG("Pas assez d'arguments - min 1");
 		return NULL;
@@ -26,16 +26,24 @@ object* moins (object* o){
 	object* obj_sous = make_object();
 	obj_sous->type = SFS_NUMBER;
 	obj_sous->this.number.this.integer = 0;
-	do
+	if (cdr(o) == obj_empty_list) // Si 1 élément: on renvoie l'inverse
+	{ 
+		obj_sous= - car(o)->this.number.this.integer;
+		return obj_sous;
+	}
+	else
 	{
-		//verif qu'on atteint pas la limite -----------------------A VERIFIER
-		if (INT_MIN+o.this.number.this.integer > obj_sous)
-		{
-			WARNING_MSG("Overflow"); // message mais l'opération continue
-		}		
-		obj_sous->this.num = car(o)->this.number.this.integer- obj_sous->this.num.this.integer;
+		obj_sous->this.number.this.integer = car(o)->this.number.this.integer - car(cdr(o))->this.number.this.integer;
 		o = cdr(o);
-	}while (cdr(cdr(o)) != obj_empty_list)
+		while (cdr(o) != obj_empty_list)
+		{
+			if ( (INT_MIN + o->this.number.this.integer) > obj_sous->this.number.this.integer) //verif qu'on atteint pas la limite
+			{
+				WARNING_MSG("Overflow"); // message mais l'opération continue
+			}		
+		obj_sous->this.num = obj_sous->this.num.this.integer - car(o)->this.number.this.integer; //(- a b) = a-b
+		o = cdr(o);
+		}
 	//verif que la soustraction est un integer-----------------------A VERIFIER
 	return obj_sous;
 }
@@ -72,7 +80,7 @@ object* plus (object* o){
 		}		
 		obj_somme->this.number.this.integer = car(o)->this.number.this.integer+ obj_somme->this.number.this.integer;
 		o = cdr(o);
-	}while (cdr(cdr(o)) != obj_empty_list)
+	}while (o != obj_empty_list)
 	//verif que l'addition est un integer-----------------------A VERIFIER
 	return obj_somme;
 }
@@ -112,7 +120,7 @@ object* produit (object* o){
 		}		
 		obj_produit->this.number.this.integer = car(o)->this.number.this.integer* obj_produit->this.number.this.integer;
 		o = cdr(o);
-	}while (cdr(cdr(o)) != obj_empty_list)
+	}while (o != obj_empty_list)
 	//verif que la multiplication est un integer-----------------------A VERIFIER
 	return obj_produit;
 }
@@ -143,7 +151,7 @@ object* quotient (object* o){
 	object* obj_divis = make_object();
 	obj_divis->type = SFS_NUMBER;
 	obj_divis->this.number.this.integer = 1;
-	if(car(cdr(o))->this.number.this.integer  == NULL)
+	if(cdr(o) == obj_empty_list)
 	{
 		obj_divis->this.number.this.integer = obj_divis->this.number.this.integer/ car(o)->this.number.this.integer;
 		return obj_divis;
@@ -152,17 +160,17 @@ object* quotient (object* o){
 	{
 		obj_divis->this.number.this.integer = car(o)->this.number.this.integer/ car(cdr(o))->this.number.this.integer;
 		o = cdr(o);
-	}
-	while (cdr(o) != obj_empty_list)
-	{
-		//verif qu'on atteint pas la limite -----------------------A VERIFIER
-		if(obj_divis->this.number.this.integer  == 0)
+		while (cdr(o) != obj_empty_list)
 		{
-			WARNING_MSG("Division par 0"); // message mais l'opération continue
-			return NULL;
+			//verif qu'on atteint pas la limite -----------------------A VERIFIER
+			if(obj_divis->this.number.this.integer  == 0)
+			{
+				WARNING_MSG("Division par 0"); // message mais l'opération continue
+				return NULL;
+			}
+			obj_divis->this.number.this.integer =obj_divis->this.number.this.integer /  car(o)->this.number.this.integer;
+			o = cdr(o);
 		}
-		obj_divis->this.number.this.integer = car(o)->this.number.this.integer/ obj_divis->this.number.this.integer;
-		o = cdr(o);
 	}
 	//verif que la division est un integer-----------------------A VERIFIER
 	return obj_divis;
@@ -588,6 +596,42 @@ object* isstring (object* o){
 	}while (cdr(cdr(o)) != obj_empty_list);
 	return obj_res;
 }	
+		    
+		    
+		    
+		    
+/**
+*@fn object* ispair (object* o)
+*
+*@brief 
+*
+*@param object* o pointeur vers la structure étudiée
+*
+*@return object o* retourne l'expression passée dans la fonction
+*/
+
+
+object* ispair (object* o){ 
+	if (o == obj_empty_list)
+	{ 
+		WARNING_MSG("Pas assez d'arguments - min 1");
+		return NULL;
+	}
+	do
+	{ // ------------------------ A VERIFIER
+		if (car(o)->this.type == SFS_PAIR)
+		{
+			obj_res=obj_true;
+			o = cdr(o);
+		}
+		else
+		{
+			obj_res=obj_false;
+			return obj_res
+		}
+	}while (cdr(cdr(o)) != obj_empty_list);
+	return obj_res;
+}	
 
 
 /***********************************************************************************************************
@@ -688,7 +732,22 @@ object* cdr (object* o)	// ---------------- a tester si o est une liste
 	
 object* set-car! (object* o)	
 {
-	// -------------------- A FAIRE
+	if ((o == obj_empty_list) || (cdr(o) == obj_empty_list))
+	{ 
+		WARNING_MSG("Pas assez d'arguments - min 2");
+		return NULL;
+	}
+	if ( o->this.type != SFS_PAIR )
+	{
+		WARNING_MSG("set-car! ne s'applique pas à votre argument");
+		return NULL;
+	}
+	object* obj_setcar = make_object();
+	obj_setcar->type = SFS_PAIR;
+	// ------------------- VERIFIER QUE LES ELEMENTS SONT BIEN DES NOMBRES?
+	obj_setcar->this.pair.car= cdr(o)->this.number.this.integer;
+	obj_setcar->this.pair.cdr= cdr(car(o))->this.number.this.integer;
+	return obj_setcar;
 }
 	
 	
@@ -705,5 +764,184 @@ object* set-car! (object* o)
 	
 object* set-cdr! (object* o)	
 {
-	// -------------------- A FAIRE	
+	if ((o == obj_empty_list) || (cdr(o) == obj_empty_list))
+	{ 
+		WARNING_MSG("Pas assez d'arguments - min 2");
+		return NULL;
+	}
+	if ( o->this.type != SFS_PAIR )
+	{
+		WARNING_MSG("set-car! ne s'applique pas à votre argument");
+		return NULL;
+	}
+	object* obj_setcdr = make_object();
+	obj_setcdr->type = SFS_PAIR;
+	// ------------------- VERIFIER QUE LES ELEMENTS SONT BIEN DES NOMBRES?
+	obj_setcdr->this.pair.car= car(car(o))->this.number.this.integer;
+	obj_setcdr->this.pair.cdr= cdr(o)->this.number.this.integer;
+	return obj_setcar;
+}
+	
+
+		    
+		    
+/**
+*@fn object* list (object* o)
+*
+*@brief 
+*
+*@param object* o pointeur vers la structure étudiée
+*
+*@return object o* retourne l'expression passée dans la fonction
+*/
+	
+	
+object* list (object* o)	
+{
+	//------------------------A FAIRE
+}
+
+		    
+		    
+		    
+		    
+/**
+*@fn object* iseq (object* o)
+*
+*@brief 
+*
+*@param object* o pointeur vers la structure étudiée
+*
+*@return object o* retourne l'expression passée dans la fonction
+*/
+	
+	
+object* iseq (object* o)	
+{
+	//------------------------A FAIRE
+}		    
+		    
+		    
+
+
+		    
+		    
+/***********************************************************************************************************
+/***********************************************************************************************************
+/**** 
+/****        Conversions de types
+/**** 
+/**** 
+
+
+
+
+/**
+*@fn object* char2integer (object* o)
+*
+*@brief 
+*
+*@param object* o pointeur vers la structure étudiée
+*
+*@return object o* retourne l'expression passée dans la fonction
+*/
+	
+	
+object* char2integer (object* o)	
+{	
+ 
+}		    
+
+
+
+
+/**
+*@fn object* integer2char (object* o)
+*
+*@brief 
+*
+*@param object* o pointeur vers la structure étudiée
+*
+*@return object o* retourne l'expression passée dans la fonction
+*/
+	
+	
+object* integer2char (object* o)	
+{	
+ 
+}		    
+
+
+
+
+/**
+*@fn object* number2string (object* o)
+*
+*@brief 
+*
+*@param object* o pointeur vers la structure étudiée
+*
+*@return object o* retourne l'expression passée dans la fonction
+*/
+	
+	
+object* number2string (object* o)	
+{	
+ 
+}		    
+
+
+
+
+/**
+*@fn object* string2number (object* o)
+*
+*@brief 
+*
+*@param object* o pointeur vers la structure étudiée
+*
+*@return object o* retourne l'expression passée dans la fonction
+*/
+	
+	
+object* string2number (object* o)	
+{	
+ 
+}			   
+
+
+
+
+/**
+*@fn object* symbol2string (object* o)
+*
+*@brief 
+*
+*@param object* o pointeur vers la structure étudiée
+*
+*@return object o* retourne l'expression passée dans la fonction
+*/
+	
+	
+object* symbol2string (object* o)	
+{	
+ 
+}		    
+
+
+
+/**
+*@fn object* string2symbol (object* o)
+*
+*@brief 
+*
+*@param object* o pointeur vers la structure étudiée
+*
+*@return object o* retourne l'expression passée dans la fonction
+*/
+	
+	
+object* string2symbol (object* o)	
+{	
+ 
 }
